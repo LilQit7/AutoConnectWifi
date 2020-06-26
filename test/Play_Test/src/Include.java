23dc422ac8affb9fd76b9df8ad7bc227cd85c1cd
LilQit7 <xiaoqi6pi@gmail.com>
2020-06-26 09:03:21 +0800

    Initial commit

---
+++ test/Play_Test/src/Include.java
@@ -0,0 +1,20 @@
+public class Include {
+    public static void main(String[] args) {
+    }
+    public void dog(){
+
+    }
+    public void cat(){
+
+    }
+
+
+    public class Animal{
+        private int _age;
+        public  string Name{
+            get
+        }
+
+    }
+
+}
